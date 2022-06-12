5f2afc80e61b682b7c254c6300d0d7376f5ed556
Nikita_Kulchytsyi <zpaulz61@gmai.com>
2022-06-12 20:02:41 +0300

    update UserService

--- services/userService.js
+++ services/userService.js
@@ -2,8 +2,6 @@ const { UserRepository } = require('../repositories/userRepository');
 
 class UserService {
 
-    // TODO: Implement methods to work with user
-
     search(search) {
         const item = UserRepository.getOne(search);
         if(!item) {
@@ -11,6 +9,47 @@ class UserService {
         }
         return item;
     }
+
+    getCollection() {
+        const item = UserRepository.getAll();
+        if(!item) {
+            return null;
+        }
+        return item;
+    }
+
+    getById(id) {
+        const item = UserRepository.getOne(user => user.id === id);
+        if(!item) {
+            return null;
+        }
+        return item;
+    }
+
+    create(user) {
+        const item = UserRepository.create(user)
+        if(!item) {
+            return null;
+        }
+        return item;
+    }
+
+    update(id, userToUpdate) {
+        const item = UserRepository.update(id, userToUpdate)
+        if(!item) {
+            return null;
+        }
+        return item;
+    }
+
+    delete(id) {
+        const item = UserRepository.delete(id)
+        if(!item) {
+            return null;
+        }
+        return item;
+    }
+
 }
 
 module.exports = new UserService();
