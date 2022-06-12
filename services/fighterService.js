177192d96d09aedad0838c680dd577d1e572f6fc
Nikita_Kulchytsyi <zpaulz61@gmai.com>
2022-06-12 21:11:15 +0300

    add Fighter routes

--- services/fighterService.js
+++ services/fighterService.js
@@ -2,6 +2,46 @@ const { FighterRepository } = require('../repositories/fighterRepository');
 
 class FighterService {
     // TODO: Implement methods to work with fighters
+
+    getCollection() {
+        const result = FighterRepository.getAll();
+        if(!result) {
+            return null;
+        }
+        return result;
+    }
+
+    getById(id) {
+        const result = FighterRepository.getOne(fighter => fighter.id === id);
+        if(!result) {
+            return null;
+        }
+        return result;
+    }
+
+    create(fighter) {
+        const result = FighterRepository.create(fighter)
+        if(!result) {
+            return null;
+        }
+        return result;
+    }
+
+    update(id, fighterToUpdate) {
+        const result = FighterRepository.update(id, fighterToUpdate)
+        if(!result) {
+            return null;
+        }
+        return result;
+    }
+
+    delete(id) {
+        const result = FighterRepository.delete(id)
+        if(!result) {
+            return null;
+        }
+        return result;
+    }
 }
 
 module.exports = new FighterService();
