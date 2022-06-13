bb4cdb67bdd3882964950b44f04a9b12a67171d4
Nikita_Kulchytsyi <zpaulz61@gmai.com>
2022-06-13 16:35:03 +0300

    fighter/user validation middleweare

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -1,14 +1,47 @@
 const { fighter } = require('../models/fighter');
+const { createError } = require('../helpers/createErrorHelper');
+const { creationModelValidation } = require('../helpers/modelValidationHelpers');
+const { powerValidation, defenseValidation, healthValidation } = require('../helpers/fighterValidationHelpers');
 
-const createFighterValid = (req, res, next) => {
-    // TODO: Implement validatior for fighter entity during creation
+
+const createFighterValidation = (req, res, next) => {
+    const isValidModel = creationModelValidation(fighter, req.body);
+    if (!isValidModel) {
+        res.status(400);
+        res.err = createError('Invalid model');
+        return next();
+    }
+
+    const isValidPower = powerValidation(req.body.power);
+    if (!isValidPower) {
+        res.status(400);
+        res.err = createError('Invalid 1-99 power');
+        return next();
+    }
+
+    const isValidDefense = defenseValidation(req.body.defense);
+    if (!isValidDefense) {
+        res.status(400);
+        res.err = createError('Invalid 1-9 defense');
+        return next();
+    }
+
+    const isValidHealth = healthValidation(req.body.health);
+    if (!isValidHealth) {
+        res.status(400);
+        res.err = createError('Invalid 81-119 health');
+        return next();
+    }
+
+
+
     next();
 }
 
-const updateFighterValid = (req, res, next) => {
+const updateFighterValidation = (req, res, next) => {
     // TODO: Implement validatior for fighter entity during update
     next();
 }
 
-exports.createFighterValid = createFighterValid;
-exports.updateFighterValid = updateFighterValid;
+exports.createFighterValidation = createFighterValidation;
+exports.updateFighterValidation = updateFighterValidation;
--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,15 +1,52 @@
+const { createError } = require('../helpers/createErrorHelper');
+const { creationModelValidation } = require('../helpers/modelValidationHelpers');
+const { emailValidation, phoneNumberValidation, passwordValidation } = require('../helpers/userValidationHelper');
 const { user } = require('../models/user');
-const createUserValid = (req, res, next) => {
-    // TODO: Implement validatior for user entity during creation
+
+const createUserValidation = (req, res, next) => {
+    const isValidModel = creationModelValidation(user, req.body);
+    
+    if (!isValidModel) {
+        res.status(400);
+        res.err = createError('Invalid model');
+        return next();
+    }
+
+    const isValidEmail = emailValidation(req.body.email)
+    
+    if (!isValidEmail) {
+        res.status(400)
+        res.err = createError('Invalid gmail address');
+        return next();
+    }
+
+    const isValidPhoneNumber = phoneNumberValidation(req.body.phoneNumber)
+
+    if (!isValidPhoneNumber) {
+        res.status(400)
+        res.err = createError('Invalid phone number');
+        return next();
+    }
+
+    const isValidPassword = passwordValidation(req.body.password)
+    console.log(isValidPassword)
+    if (!isValidPassword) {
+        res.status(400)
+        res.err = createError('Invalid password');
+        return next();
+    }
+
 
     next();
+
+    
 }
 
-const updateUserValid = (req, res, next) => {
+const updateUserValidation = (req, res, next) => {
     // TODO: Implement validatior for user entity during update
 
     next();
 }
 
-exports.createUserValid = createUserValid;
-exports.updateUserValid = updateUserValid;
+exports.createUserValidation = createUserValidation;
+exports.updateUserValidation = updateUserValidation;
